be2e2fe934210c50a53c589fd8c79e3d4b944d7d
devAxri <244042090+devAxri@users.noreply.github.com>
2026-05-27 22:33:33 +0200

    You're right Rikko, there is a method called .values()

---
+++ 15-valid-parentheses/rev2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+        seen = []
+
+        for char in s:
+            if char in parentheses.values():
+                seen.append(char)
+            else: # we can assume its a closing one
+                if len(seen) == 0:
+                    return False
+                if seen[-1] != parentheses[char]:
+                    return False
+
+                seen.pop()
+
+        #print(seen)
+        return seen == []
+        
+sol = Solution()
+print(sol.isValid("([{}])"))
